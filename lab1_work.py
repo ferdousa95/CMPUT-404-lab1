c6af55cce5aec78a793658a7f45b5d226d7a91d7
ferdousa95 <72733638+ferdousa95@users.noreply.github.com>
2023-01-09 19:06:08 -0700

    Update lab1_work.py
    Bring the comments back, no changes to the code.

--- lab1_work.py
+++ lab1_work.py
@@ -3,6 +3,8 @@ import requests
 """
 Leaving commented code for TA if they needs to check earlier examples. 
 """
+# x = requests.get('http://www.google.com')
+# print(x.status_code)
 
 url = 'https://raw.githubusercontent.com/ferdousa95/CMPUT-404-lab1/main/lab1_work.py'
 print(requests.get(url).text)
